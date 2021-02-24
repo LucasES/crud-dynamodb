fa444115792807cbe01b099078643ba518491909
Lucas Araújo <lucas.compufc@gmail.com>
2021-02-24 20:09:14 -0300

    Initial commit

---
+++ src/main/java/com/cruddynamodb/CrudDynamodbApplication.java
@@ -0,0 +1,13 @@
+package com.cruddynamodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudDynamodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudDynamodbApplication.class, args);
+	}
+
+}
